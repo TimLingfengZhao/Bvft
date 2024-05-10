e8743e20908f5f767ca21a15f3499bf1ed7680d6
LingfengZhao <lz20@illinois.edu>
2024-05-10 15:39:11 -0500

    adjust the wideth and height of the top k cal, the debug graph

--- RL_saved_data/top_k_cal.py
+++ RL_saved_data/top_k_cal.py
@@ -654,8 +654,9 @@ def sort_lists_by_first_dec(list_to_sort, names_list):
     return list(sorted_list_to_sort), list(sorted_names_list)
 
 def draw_debug_graph(y_value_list, name_list, line_name_list):
-
-    plt.figure(figsize=(15,8))
+    wide = int(len(name_list))
+    high = (int(wide / 2)) + 1
+    plt.figure(figsize=(wide, high))
 
     for i in range(len(line_name_list)):
         plt.plot(name_list, [y[i] for y in y_value_list], label=line_name_list[i])
