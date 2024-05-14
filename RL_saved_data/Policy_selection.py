956aa22495a9da7eaf0f71dc1877a9e778411cef
LingfengZhao <lz20@illinois.edu>
2024-05-14 18:51:20 -0500

    adjust the wideth and height of the top k cal, the debug graph

--- RL_saved_data/Policy_selection.py
+++ RL_saved_data/Policy_selection.py
@@ -441,6 +441,7 @@ initial_state = 12345
 data_saving_path = ["Bvft_ranking"]
 bvft_obj = Bvft_poli(device, data_saving_path, whole_dataset,env,k,num_runs,FQE_saving_step_list,initial_state)
 
-bvft_obj.select_Q()
+# bvft_obj.select_Q()
 bvft_obj.calculate_k(data_saving_path,self.data_saving_path,self.FQE_saving_step_list,self.initial_state,self.k,self.num_runs)
+bvft_obj.run()
 
