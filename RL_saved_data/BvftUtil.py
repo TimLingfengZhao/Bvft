6d5c427dc96490edd161dad0c9ec0e99a3eeb0f6
LingfengZhao <lz20@illinois.edu>
2024-05-21 13:18:44 -0500

    remove duplicateds

--- RL_saved_data/BvftUtil.py
+++ RL_saved_data/BvftUtil.py
@@ -295,8 +295,7 @@ class CustomDataLoader:
         padded_next_states = np.append(padded_next_states, [self.dataset.episodes[iteration_number].observations[-1]], axis=0)
         rewards = self.dataset.episodes[iteration_number].rewards
         done = self.dataset.episodes[iteration_number].terminated
-        print("length next state : ",len(padded_next_states))
-        print("length state : ",len(states))
+        print("states : ",states)
         sys.exit()
         return states, actions, padded_next_states, rewards, done
 
