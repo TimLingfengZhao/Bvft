c9d35f66236f7ed2d6b61138652bff7d16bde751
LingfengZhao <lz20@illinois.edu>
2024-06-04 20:49:11 -0500

    the good version for abs class

--- RL_saved_data/Policy_selection.py
+++ RL_saved_data/Policy_selection.py
@@ -72,6 +72,34 @@ class policy_select(ABC):
         self.trajectory_num = len(self.test_episodes)
         self.data_size = self.get_data_size(test_episodes)
 
+    def draw_Bvft_resolution_loss_graph(self,Bvft_final_resolution_loss, FQE_step_Q_list, resolution_list, save_folder_name,
+                                        line_name_list, group_list):
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        x_list = []
+
+        for i in range(len(resolution_list)):
+            x_list.append(str(resolution_list[i]) + "res" + "_" + str(group_list[i]) + "groups")
+        x_positions = list(range(len(x_list)))
+        for index, y_values in enumerate(Bvft_final_resolution_loss):
+            ax.plot(x_positions, y_values, label=line_name_list[index])
+
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.3), ncol=3)
+        ax.set_xticks(x_positions)  # Set the x positions
+        ax.set_xticklabels(x_list, rotation=45, ha="right")
+        ax.set_title("mean loss with different policy")
+        ax.set_ylabel('Bvft_loss')
+        ax.set_xlabel('resolutions_groups')
+
+        Policy_ranking_saving_place = "Policy_ranking_saving_place"
+        Policy_k_saving_place = "Policy_k_saving_place"
+        Res_plot_folder = "Bvft_res_plot_"+str(resolution_list)
+        if not os.path.exists(Res_plot_folder):
+            os.makedirs(Res_plot_folder)
+        plot_name = "Figure_1R_" + save_folder_name  + ".png"
+        save_path = os.path.join(Res_plot_folder, plot_name)
+        plt.savefig(save_path, bbox_inches='tight')
+        plt.close()
     def normalized_mean_square_error_with_error_bar(self,actual, predicted, NMSE_normalization_factor):
 
         if len(actual) != len(predicted):
@@ -775,19 +803,46 @@ class Bvft_poli(policy_select):
         resolution_list = [2, 3, 4, 8, 16, 100, 1e10]
         rmax, rmin = self.env.reward_range[0], self.env.reward_range[1]
         result_list = []
+        Bvft_final_resolution_loss = []
+        group_list = []
         for resolution in resolution_list:
             record = BvftRecord()
             bvft_instance = BVFT(q_functions, self.test_data, gamma, rmax, rmin, policy_name_listi, record,
                                  "torch_actor_critic_cont", verbose=True, data_size=self.data_size,
                                  trajectory_num=self.trajectory_num)
-            # print("resolution : ",resolution)
             bvft_instance.run(resolution=resolution)
-            # for i in range(len(record.losses[0])):
-            #     Bvft_final_resolution_loss[i].append(record.losses[0][i])
+            group_list.append(record.group_counts[0])
+            for i in range(len(record.losses[0])):
+                Bvft_final_resolution_loss[i].append(record.losses[0][i])
 
             result_list.append(record.losses[0])
         min_loss_list = self.get_min_loss(result_list)
         less_index_list = self.rank_elements_lower_higher(min_loss_list)
+
+        FQE_lr_list = [1e-4, 2e-5]
+        FQE_hl_list = [[128, 256], [128, 1024]]
+        line_name_list = []
+        for i in range(len(self.FQE_saving_step_list)):
+            for j in range(len(FQE_lr_list)):
+                for k in range(len(FQE_hl_list)):
+                    line_name_list.append('FQE_' + str(FQE_lr_list[j]) + '_' + str(FQE_hl_list[k]) + '_' + str(
+                        self.FQE_saving_step_list[i]) + "step")
+        Policy_ranking_saving_place = "Policy_ranking_saving_place"
+        Policy_k_saving_place = "Policy_k_saving_place"
+        Res_plot_folder = "Bvft_res_data_"+str(resolution_list)
+
+        Policy_k_saving_path = os.path.join(Policy_ranking_saving_place,Policy_k_saving_place)
+        Res_plot_saving_folder = os.path.join(Policy_k_saving_path,Res_plot_folder)
+        save_folder_name = policy_name_listi
+        if not os.path.exists(Res_plot_saving_folder):
+            os.makedirs(Res_plot_saving_folder)
+        policy_saving_path = os.path.join(Res_plot_saving_folder,policy_name_listi)
+        if not os.path.exists(policy_saving_path):
+            os.makedirs(policy_saving_path)
+        self.save_as_pkl(policy_saving_path,Bvft_final_resolution_loss)
+        self.save_as_txt(policy_saving_path,Bvft_final_resolution_loss)
+        self.draw_Bvft_resolution_loss_graph(Bvft_final_resolution_loss, self.FQE_saving_step_list, resolution_list,
+                                        save_folder_name, line_name_list, group_list)
         return less_index_list
 class Bvft_zero(policy_select):
     def select_Q(self,q_functions,q_name_functions,policy_name_listi,Q_sa,r_plus_vfsp):
@@ -805,10 +860,8 @@ class Bvft_zero(policy_select):
             #     Bvft_final_resolution_loss[i].append(record.losses[0][i])
 
             result_list.append(record.losses[0])
-        print("result list : ",result_list)
-        less_index_list = self.rank_elements_lower_higher(result_list)
-        print("less index list : ",less_index_list)
-        sys.exit()
+        list_to_rank = result_list[0]
+        less_index_list = self.rank_elements_lower_higher(list_to_rank)
         return less_index_list
 
 
