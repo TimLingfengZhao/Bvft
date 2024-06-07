c5136f545334c4258354d63a379ccb3693b4a8a2
LingfengZhao <lz20@illinois.edu>
2024-06-07 16:36:51 -0500

    the good version for abs class

--- Env_changing/Exp_run_hopper.py
+++ Env_changing/Exp_run_hopper.py
@@ -2,7 +2,7 @@ from Env_change_util import *
 import gymnasium
 # env = gymnasium.make("Hopper-v4")
 # print(env.unwrapped.model.opt)  # change this
-env = gym.make("Hopper-v4")
+env = gymnasium.make("Hopper-v4")
 print(env.unwrapped.model.opt)
 env.unwrapped.model.opt.gravity = np.array([0.0,0.0,-1002])
 print(env.unwrapped.model.opt)
