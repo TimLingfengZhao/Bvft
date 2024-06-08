a48f7d790ed186e98b249921cad9a4dd2ba890f9
LingfengZhao <lz20@illinois.edu>
2024-06-08 16:56:09 -0500

    the good version for abs class

--- Env_changing/Exp_run_hopper.py
+++ Env_changing/Exp_run_hopper.py
@@ -17,31 +17,16 @@ parameter_list =[[np.array([0.0,0.0,-4.9]),
                   np.array([0.0, 0.0, 10.0])]
                  ]
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-policy_parameter_map = {"policy_total_step":300000,
-                 "policy_episode_step":10000,
-                        "policy_saving_number" : 5,"policy_learning_rate":0.0001,"policy_hidden_layer":[64,256],
+policy_parameter_map = {"policy_total_step":3000,
+                 "policy_episode_step":1000,
+                        "policy_saving_number" : 3,"policy_learning_rate":0.0001,"policy_hidden_layer":[64,256],
                         "algorithm_name_list":["DDPG","SAC"]}
-# parameter_name_list = ["gravity","magnetic","wind"]
-# hopper_exp = Hopper_edi(device=device,parameter_list=parameter_list,
-#                         parameter_name_list=parameter_name_list,env_name="Hopper-v4")
-# hopper_exp.train_policy()
-policy_hidden_list = [64, 256]
-policy_rate = 0.0001
-ddpg_class = getattr(d3rlpy.algos, "DDPGConfig")
-ddpg = ddpg_class(
-    actor_encoder_factory=d3rlpy.models.VectorEncoderFactory(hidden_units=policy_hidden_list),
-    critic_encoder_factory=d3rlpy.models.VectorEncoderFactory(hidden_units=policy_hidden_list),
-    actor_learning_rate=policy_rate,
-    critic_learning_rate=policy_rate,
-).create(device=device)
-buffer = d3rlpy.dataset.create_fifo_replay_buffer(limit=1000000, env=env)
-explorer = d3rlpy.algos.ConstantEpsilonGreedy(0.3)
-ddpg.fit_online(env=env,
-                buffer = buffer,
-                explorer=explorer,
-                n_steps = 300000,
-                eval_env = env,
-                n_steps_per_epoch=1000,
-                update_start_step=1000,
-                with_timestamp=False,
-                )
+
+
+
+parameter_name_list = ["gravity","magnetic","wind"]
+hopper_exp = Hopper_edi(device=device,parameter_list=parameter_list,
+                        parameter_name_list=parameter_name_list,policy_training_parameter_map=policy_parameter_map,
+                        env_name="Hopper-v4")
+hopper_exp.train_policy()
+
