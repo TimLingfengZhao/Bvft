74732381ba97c0f1778dbda2417e3c72cd471060
LingfengZhao <lz20@illinois.edu>
2024-06-11 21:26:03 -0500

    the good version for abs class

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -158,12 +158,12 @@ class Hopper_edi(ABC):
         return True
     def generate_offline_data(self,trajectory_numbers):
         print("parameter of environments in current class : ")
-        for key, value in self.para_map:
+        for key, value in self.para_map.items():
             print(f"{key}: {value}")
         true_env_number = input("Please enter the environment parameter number you choose")
         print(true_env_number)
     def print_environment_parameters(self):
-        for key, value in self.para_map:
+        for key, value in self.para_map.items():
             print(f"{key}: {value}")
 
     def train_policy(self):
