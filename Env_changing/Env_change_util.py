293e9c567f92ab8accfe92212bdc58e492cd8c51
LingfengZhao <lz20@illinois.edu>
2024-06-11 22:27:47 -0500

    the good version for abs class

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -208,7 +208,7 @@ class Hopper_edi(ABC):
         for i in range(trajectory_numbers):
             one_episode_data = self.generate_one_trajectory(true_env_number,max_time_step,algorithm_name)
             final_data.append(one_episode_data)
-            for key, value in self.one_episode_data.items():
+            for key, value in one_episode_data.items():
                 print(f"{key}: {value}")
 
 
