67a1c52a4ac48a016f893855f3f8ce1daeb469ff
LingfengZhao <lz20@illinois.edu>
2024-06-18 17:17:49 +0800

    debug sample

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -431,7 +431,7 @@ class Hopper_edi(ABC):
                     state = ui[0]
                     reward = ui[1]
                     done = ui[2]
-                    print("state after step : ",state)
+                    print("state= fter step : ",state)
                     total_rewards += reward * discount_factor
                     discount_factor *= self.gamma
                     num_step += 1
