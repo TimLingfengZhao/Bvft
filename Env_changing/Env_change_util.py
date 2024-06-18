b7324507c0f22b58ba994e9bb592a6457e771c98
LingfengZhao <lz20@illinois.edu>
2024-06-18 17:23:05 +0800

    debug sample

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -417,6 +417,7 @@ class Hopper_edi(ABC):
                 # print("len actions : ",len(actions))
                 observation =states[i]
                 action = actions[i]
+                env.sim.set_state(observation)
                 print("observations : ",observation)
                 print("action : ",action)
                 ui = env.step(action)
