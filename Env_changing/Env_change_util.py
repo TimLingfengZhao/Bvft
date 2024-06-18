0f84dcd099f48db8543d503ff17b443d8ce6d459
LingfengZhao <lz20@illinois.edu>
2024-06-18 16:12:48 +0800

    no oooooo

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -448,7 +448,7 @@ class Hopper_edi(ABC):
                     self.r_plus_vfsp[(i+1)*(j+1)-1][ptr:ptr + length] = vfsp.flatten()[:length]
             ptr += 1
         print("self q_sa : ",self.q_sa)
-        print("lengthe e  q sa : ",len(self.q_sa))
+        print("lengthe e e  q sa : ",len(self.q_sa))
 
 
 
