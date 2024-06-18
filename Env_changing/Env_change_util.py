b289fc519c9b2c992b91688b8eca3a94aba42496
LingfengZhao <lz20@illinois.edu>
2024-06-19 05:50:35 +0800

    debug sample

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -417,7 +417,7 @@ class Hopper_edi(ABC):
                 # print("len actions : ",len(actions))
                 observation =states[i]
                 action = actions[i]
-                print(env.unwrapped.model。opt)
+                print(env.unwrapped.model.opt)
                 env.unwrapped.set_state(observation)
                 print("observations : ",observation)
                 print("action : ",action)
