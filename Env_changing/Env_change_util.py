661c87be665619c8c4e20dcd17da3659ebba4d1f
LingfengZhao <lz20@illinois.edu>
2024-06-19 06:19:06 +0800

    debug sample

--- Env_changing/Env_change_util.py
+++ Env_changing/Env_change_util.py
@@ -425,7 +425,7 @@ class Hopper_edi(ABC):
                 action = actions[i]
                 env.reset()
                 env.state= observation
-                print("env 2 state :",env.state)
+                print("env 2 ustate :",env.state)
                 sys.exit()
                 print("observations : ",observation)
                 print("action : ",action)
