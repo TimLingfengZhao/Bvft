dfae3063c710ca68f33aa1761ae24dc15185ff7b
LingfengZhao <lz20@illinois.edu>
2024-06-20 22:33:58 +0800

    debug sample

--- Env_changing/Exp_run_hopper.py
+++ Env_changing/Exp_run_hopper.py
@@ -16,15 +16,15 @@ parameter_list = [
     [gravity[0], magnetic[2], wind[0]]
 ]
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-policy_parameter_map = {"policy_total_step":300000,
-                 "policy_episode_step":10000,
+policy_parameter_map = {"policy_total_step":30000,
+                 "policy_episode_step":1000,
                         "policy_saving_number" : 30,"policy_learning_rate":0.0001,"policy_hidden_layer":[64,256],
                         "algorithm_name_list":["DDPG"]}
 parameter_name_list = ["gravity","magnetic","wind"]
 
 common_params = {
-    "gamma": 0.99, "trajectory_num" : 200,
-"max_timestep" : 1000, "total_select_env_number" : 2,
+    "gamma": 0.99, "trajectory_num" : 10,
+"max_timestep" : 100, "total_select_env_number" : 2,
 "env_name" : "Hopper-v4"
 }
 
